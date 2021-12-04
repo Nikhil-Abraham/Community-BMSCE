a75440f30c72990f0fda04351b6d53a64aff1dc3
Nikhil Abraham <nikhil.abraham@orangepower.co.uk>
2021-12-04 17:26:41 +0530

    commit

--- community/src/App.js
+++ community/src/App.js
@@ -1,10 +1,12 @@
-import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Community BMSCE</h1>
+        <Login />
+        <Header />
       </header>
     </div>
   );
