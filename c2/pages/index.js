4dccb82b57d4b8a1e158b8558cf0f36e0b8de8bc
Nayana J <68220099+Nayanaj117@users.noreply.github.com>
2021-12-10 18:09:07 +0530

    Update index.js

--- c2/pages/index.js
+++ c2/pages/index.js
@@ -2,13 +2,35 @@ import Head from "next/head";
 import Image from "next/image";
 import Header from "../Components/Header";
 
+
 export default function Home() {
   return (
-    <div class="bg-custom_white h-screen">
+    <div class="bg-custom_white h-screen  text-custom_grey-150 font-Nato font-semibold   ">
       <Header />
-      <div class="text-custom_grey-150 font-Nato font-semibold text-10xl">
-        Community
-      </div>
+
+
+        <div id="register_area-jq" class="container  punchline flex flex-col items-center bg-custom_blue w-full h-screen mb-8 ... ">
+
+
+            <div class="py-12 ... mt-15 ... text-xl ..">LOGIN</div>
+      <form action="/intro" method="post"  name="login"  >
+        <div class="form-group py-6 ...">
+          <label class="form-label" for="email">Enter your email:</label><br/>
+          <input   type="email" class="form-control " placeholder="Enter email" id="email">
+          </input>
+          </div>
+        <div class="form-group py-6 ...">
+          <label class="form-label" for="pwd">Password:</label><br/>
+          <input  type="password" class="form-control " placeholder="Enter password" id="pwd">
+          </input>
+          </div>
+        <div class = "register-btn  bg-custom_black hover:bg-custom_black mt-9 ... w-20 ... h-9 ... ml-36 ...">
+        <a href="">LOGIN</a>
+          </div>
+      </form>
     </div>
+
+  </div>
+
   );
 }
