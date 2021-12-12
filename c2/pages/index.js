8fcc3d216534b403c2a839aca6493d35f423c384
Nayana J <68220099+Nayanaj117@users.noreply.github.com>
2021-12-12 22:43:33 +0530

    Update index.js

--- c2/pages/index.js
+++ c2/pages/index.js
@@ -9,25 +9,28 @@ export default function Home() {
       <Header />
 
 
-        <div id="register_area-jq" class="container  punchline flex flex-col items-center bg-custom_blue w-full h-screen mb-8 ... ">
+        <div id="register_area-jq" class="container  punchline flex flex-col items-center
+                bg-custom_pink w-full h-screen mb-8 ...    ">
 
 
-            <div class="py-12 ... mt-15 ... text-xl ..">LOGIN</div>
-      <form action="/intro" method="post"  name="login"  >
-        <div class="form-group py-6 ...">
+            <div class="py-6 mx-20 mt-6  text-xl ...">LOGIN</div>
+            <div class=" mx-20 border-solid border-4 border-custom_grey-500/50 rounded-lg w-1/4 p-8 ...  ">
+      <form action="/intro" method="post"  name="register-form"  >
+        <div class="form-group px-6 py-6 ...">
           <label class="form-label" for="email">Enter your email:</label><br/>
-          <input   type="email" class="form-control " placeholder="Enter email" id="email">
+          <input   type="email" class="form-control mt-1 rounded-lg ..." placeholder="    Enter email" id="email">
           </input>
           </div>
-        <div class="form-group py-6 ...">
+        <div class="form-group px-6 py-6 ...">
           <label class="form-label" for="pwd">Password:</label><br/>
-          <input  type="password" class="form-control " placeholder="Enter password" id="pwd">
+          <input  type="password" class="form-control mt-1 rounded-lg ..." placeholder="    Enter password" id="pwd">
           </input>
           </div>
-        <div class = "register-btn  bg-custom_black hover:bg-custom_black mt-9 ... w-20 ... h-9 ... ml-36 ...">
-        <a href="">LOGIN</a>
+        <div class = "register-btn  bg-custom_blue hover:bg-custom_cyan  rounded-lg mx-20 mt-6 w-20 h-8 px-3 p-1 ...">
+        <a href="">Login</a>
           </div>
       </form>
+      </div>
     </div>
 
   </div>
