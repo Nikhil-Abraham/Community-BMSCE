cbec3eb326aa2fd47dce9144da0e380f444eea15
Nayana J <68220099+Nayanaj117@users.noreply.github.com>
2021-12-15 23:47:09 +0530

    Update home.js

--- c2/pages/home.js
+++ c2/pages/home.js
@@ -28,16 +28,7 @@ export default function Home() {
                 </div>
             </nav>
             </div>
-            <div>
-            <li class="flex space-y-4">
-      <a href="/new" class="hover:border-black-500 hover:border-solid hover:bg-pink hover:text-black-500 group w-full flex flex-col items-center justify-center rounded-md border-2 border-dashed border-gray-300 text-sm leading-6 text-gray-900 font-medium py-3 space-x-4">
-        <svg class="group-hover:text-black-500 mb-1 text-gray-400 space-x-4" width="20" height="20" fill="currentColor" aria-hidden="true">
-          <path d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z" />
-        </svg>
-        New project
-      </a>
-    </li>
-    </div>
+            <div>PROJECT:</div>
     <div>ALUMINI:</div>
       <div>TEACHER:</div>
         </div>
