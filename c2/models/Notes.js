6c5e8dc7119f2f71224873f5fe665b25dc8be7df
Neha <nehac.cs19@bmsce.ac.in>
2021-12-22 00:15:10 +0530

    backend for sign up attempt

--- c2/models/Notes.js
+++ c2/models/Notes.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 
 const NoteSchema = new mongoose.Schema({
-  tite: { type: string, required: true, unique: true, trim: true },
+  title: {
+     type: string, 
+     required: true,
+     unique: true,
+     trim: true },
 });
 
 module.exports = mongoose.models.Note || mongoose.model("Note", NoteSchema);
