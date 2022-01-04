af8be2989a65ee45c5e9d4808f73284aa36b4965
Nikhil Abraham <nikhil.abraham@orangepower.co.uk>
2022-01-04 22:54:43 +0530

    feat(communiti): Added login and register server side code for api

--- test/server/models/user.model.js
+++ test/server/models/user.model.js
@@ -1,10 +1,20 @@
 const mongoose = require("mongoose");
 
-const userSchemma = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+const userSchemma = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, min: 6 },
+    profilePicture: { type: String, default: "" },
+    isAlumn: { type: Boolean, default: false },
+    isAdmin: { type: Boolean, default: false },
+    bio: { type: String, max: 50, default: "" },
+    sem: { type: Number },
+    github: { type: String, default: "" },
+    linkedIn: { type: String, default: "" },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("UserData", userSchemma, "user-data");
 
---
+++ test/server/routes/users.js
@@ -0,0 +1,14 @@
+const User = require("../models/user.model");
+const router = require("express").Router();
+
+// Upddate User
+router.get("/", (req, res) => {
+  res.send("user route");
+});
+
+// Delete UserData
+// Get User
+// Follow a user
+// Unfollow a user
+
+module.exports = router;
