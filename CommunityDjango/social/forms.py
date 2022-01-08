baf50012623d7c6f32fc0711d24897b6171b5a58
Neha <nehac.cs19@bmsce.ac.in>
2022-01-08 14:18:20 +0530

    added social

---
+++ CommunityDjango/social/forms.py
@@ -0,0 +1,89 @@
+
+from django import forms
+from django.forms.widgets import Widget
+from .models import Post, PostArticle
+from landing.models import Community_User
+
+class PostFeedForm(forms.ModelForm):
+  
+  body = forms.CharField(
+    label='',
+    widget=forms.TextInput(attrs={
+      'row':'3',
+      'placeholder' : 'Say Something...',
+      'class' : 'message-field',
+    })
+  )
+
+
+  class Meta:
+    model = Post
+    fields = ['body']
+
+
+class PostArticleForm(forms.ModelForm):
+  title = forms.CharField(
+    label='',
+    widget=forms.TextInput(attrs={
+     'placeholder' :'Add Title ...',
+     'class' : 'title_field',
+    })
+  )
+  
+  body = forms.CharField(
+    label='',
+    widget=forms.Textarea(attrs={
+      'rows' : '10',
+      'placeholder' : 'Say Something...',
+      'class' : 'article_area_field',
+    })
+  )
+  article_pic = forms.ImageField(
+    label='',
+  )
+
+  class Meta:
+    model = PostArticle
+    fields = ['title','body','article_pic']
+
+class UserForm(forms.ModelForm):
+  class Meta:
+    model = Community_User
+    fields = ['name','username','phone','bio','profile_pic']
+    exclude = ['user']
+
+
+# class CommentForm(forms.ModelForm):
+#   comment = forms.CharField(
+#     label='',
+#     widget=forms.TextInput(attrs={
+#      'placeholder' :'Add Comment...',
+#      'class' : 'text_area_field',
+#     })
+#   )
+
+
+#   class Meta:
+#     model = Comment
+#     fields = ['comment']
+
+
+# class ThreadForm(forms.Form):
+#     username = forms.CharField(label='',
+#     widget=forms.TextInput(attrs={
+#       'placeholder':'Username',
+#       'class':'message-field'
+#     }),
+#     max_length=100)
+
+# class MessageForm(forms.ModelForm):
+#     body = forms.CharField(label='', max_length=1000,
+#     widget=forms.TextInput(attrs={
+#       'placeholder':'Mesaage ...',
+#       'class':'message-input-area'
+#     })
+#     )
+
+#     class Meta:
+#         model = MessageModel
+#         fields = ['body']
--- CommunityDjango/social/views.py
+++ CommunityDjango/social/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.db.models import query
+from django.db.models import Q
+from django.shortcuts import redirect, render
+from django.db.models import Q
+from django.views import View
+from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect, request
+from django.urls import reverse
+from django.views.generic.base import View
+from django.views.generic.edit import UpdateView, DeleteView
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+from .models import  Post, PostArticle
+from landing.models import Community_User
+from .forms import PostArticleForm, PostFeedForm, UserForm
+from django.contrib.auth.models import User
 
 # Create your views here.
+@login_required(login_url='accounts:login')
+def socialHome(request):
+  if request.method == "POST":
+    posts = Post.objects.all().order_by('-created_on')
+    form = PostFeedForm(request.POST)
+    if form.is_valid():
+      new_post = form.save(commit=False)
+      new_post.author = Community_User.objects.get(user = request.user)
+      new_post.save()
+    return HttpResponseRedirect(request.path)
+
+  else:
+    posts = Post.objects.all().order_by('-created_on')
+    form = PostFeedForm()
+    users = Community_User.objects.all()
+
+    user = request.user
+    current_user = Community_User.objects.get(user = user)
+
+    context = {
+      "post_list": posts,
+      'form' : form,
+      'user': current_user,
+      'list': users,
+    }
+
+    return render(request, 'social/mbrpage.html', context)
